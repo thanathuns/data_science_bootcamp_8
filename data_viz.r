c40315e90c835a6189980f4b8b75000e7cb414d9
kunzenbom@gmail.com <kunzenbom@gmail.com>
2023-11-10 09:25:45 +0000

    add new file

---
+++ data_viz.r
@@ -0,0 +1,9 @@
+# load ggplot library
+# update this file
+
+library(ggplot2)
+
+ggplot(diamonds,aes(carat,price)) +
+  geom_point()
+
+
--- hello_world.r
+++ hello_world.r
@@ -2,3 +2,12 @@
 
 print("hello world")
 print("oppenheimer is very good movie")
+
+library(dplyr)
+mtcars %>%
+  select(1:5) %>%
+  filter(mpg > 30)
+
+mtcars %>%
+  select(mpg) %>%
+  filter(mpg < 30)
